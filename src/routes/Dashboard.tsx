bdd121d567753cb6e1bfac83f47b2a5dbb351730
Konstantin Minevich <humbl3dev@gmail.com>
2022-05-26 19:34:09 -0700

    404 dashboard

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -3,13 +3,15 @@ import { Route, Routes } from 'react-router-dom';
 import { StyledDashboardContainer } from '../styles/dashboard.styles';
 import Home from '../components/dashboard/Home';
 import UpdateName from '../components/dashboard/UpdateName';
+import NotFound from './NotFound';
 
 const Dashboard = () => {
   return (
     <StyledDashboardContainer>
       <Routes>
-        <Route index element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="update-name" element={<UpdateName />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </StyledDashboardContainer>
   );
