4adfe275299a65332e3dbfd8846e8781e1e2c42f
Konstantin Minevich <humbl3dev@gmail.com>
2022-06-01 09:58:48 -0700

    update watchlist component to show loading below header instead of full page

--- src/components/dashboard/Watchlist.component.tsx
+++ src/components/dashboard/Watchlist.component.tsx
@@ -1,33 +1,10 @@
-import { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import { useAuth } from '../../state/auth/authProvider';
-import FirestoreHelper from '../../utils/firestore/firestore.utils';
-import { Content } from '../../typings';
 import ContentList from '../content/ContentList.component';
 import Loader from '../loader/Loader.component';
 import { useData } from '../../state/data/dataProvider';
-
-const StyledWatchlistContainer = styled.section`
-  margin-top: 4rem;
-`;
+import { StyledWatchlistContainer } from './Watchlist.styles';
 
 const Watchlist = () => {
   const { watchlist, loadingWatchlist } = useData();
-
-  // useEffect(() => {
-  //   FirestoreHelper.getWatchlists(authUser?.uid!).then((data) => {
-  //     setLoading(false);
-  //     if (data?.list && data?.list.length !== 0) {
-  //       setWatchlist(data.list);
-  //     } else {
-  //       setWatchlist([]);
-  //     }
-  //   });
-  // }, []);
-
-  if (loadingWatchlist) {
-    return <Loader fullScreen={false} />;
-  }
 
   return (
     <div>
@@ -35,7 +12,7 @@ const Watchlist = () => {
         <h3>
           {watchlist.length === 0 ? `No` : watchlist.length} Item{watchlist.length === 1 ? '' : 's'} in your list
         </h3>
-        {watchlist.length > 0 ? <ContentList data={watchlist} /> : <p>You don't have any items in your list.</p>}
+        {loadingWatchlist ? <Loader fullScreen={false} /> : watchlist.length > 0 ? <ContentList data={watchlist} /> : <p>You don't have any items in your list.</p>}
       </StyledWatchlistContainer>
     </div>
   );
---
+++ src/components/dashboard/Watchlist.styles.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const StyledWatchlistContainer = styled.section`
+  padding-bottom: 6rem;
+`;
