efe8c07e9d54574c200468fe75e5da6037c4f802
Konstantin Minevich <humbl3dev@gmail.com>
2022-06-06 21:20:35 -0700

    adjust contentList grid

--- src/components/content/ContentList.styles.tsx
+++ src/components/content/ContentList.styles.tsx
@@ -4,7 +4,7 @@ export const StyledContentList = styled.section`
   display: grid;
   grid-gap: 3.2rem;
   grid-template-columns: repeat(1, 1fr);
-  @media screen and (min-width: 440px) {
+  @media screen and (min-width: 385px) {
     grid-template-columns: repeat(2, 1fr);
   }
   @media screen and (min-width: 767px) {
