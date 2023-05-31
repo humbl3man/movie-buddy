ab202179e38703e2ca8f23673c9c9a6b3c5872dd
Konstantin M <humbl3dev@gmail.com>
2023-05-31 11:22:44 -0700

    add slider to movie detail page cast section

--- src/components/content/Credits.tsx
+++ src/components/content/Credits.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { Swiper, SwiperProps, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper';
+import { FaChevronLeft as PrevIcon, FaChevronRight as NextIcon } from 'react-icons/fa';
+
+import 'swiper/css';
+// import 'swiper/css/navigation';
+
 import { MovieCast } from '../../typings';
 import { buildImageUrl } from '../../utils/content/buildImageUrl.utils';
 
@@ -12,10 +19,43 @@ const StyledCredits = styled.div`
   border-top: 1px solid var(--grey700);
 `;
 const StyledCastList = styled.div`
-  display: flex;
-  flew-wrap: nowrap;
-  align-items: flex-start;
-  overflow-y: auto;
+  /* display: flex; */
+  /* flew-wrap: nowrap; */
+  /* align-items: flex-start; */
+  /* overflow-y: auto; */
+  position: relative;
+  .swiper-button-next,
+  .swiper-button-prev {
+    position: absolute;
+    top: -70px;
+    appearance: none;
+    background: none;
+    border: 2px solid rgba(255, 255, 255, 0.5);
+    color: rgba(255, 255, 255, 0.5);
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 20px;
+    &:disabled {
+      opacity: 0.2;
+    }
+    &:hover:not(:disabled) {
+      border-color: rgba(255, 255, 255, 0.8);
+      color: rgba(255, 255, 255, 0.8);
+    }
+    &:not(:disabled) {
+      cursor: pointer;
+    }
+  }
+  .swiper-button-next {
+    right: 0;
+  }
+  .swiper-button-prev {
+    left: calc(100% - 100px);
+  }
 `;
 const StyledCastCard = styled(Link)`
   margin-right: 2rem;
@@ -51,22 +91,45 @@ const Credits: React.FC<CreditProps> = ({ data, max = 20 }) => {
     event.currentTarget.src = placeHolderImage;
   };
 
+  // TODO: responsive
+  const sliderConfig: SwiperProps = {
+    spaceBetween: 50,
+    slidesPerView: 5,
+    slidesPerGroup: 5,
+    grabCursor: true,
+    modules: [Navigation],
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
+  };
+
   return (
     <StyledCredits>
       <h2 className="h4">Cast</h2>
       <StyledCastList>
-        {cast.map((cast) => {
-          return (
-            <StyledCastCard to={`/person/${cast.id}`} key={cast.id}>
-              <img src={buildImageUrl(cast.profile_path, { posterSize: 'w185' })} onError={handleImageError} width={185} height={277} alt={cast.name} />
-              <p>
-                <strong>{cast.name}</strong>
-                <br />
-                {cast.character}
-              </p>
-            </StyledCastCard>
-          );
-        })}
+        <button className="swiper-button-prev">
+          <PrevIcon />
+        </button>
+        <button className="swiper-button-next">
+          <NextIcon />
+        </button>
+        <Swiper {...sliderConfig}>
+          {cast.map((cast) => {
+            return (
+              <SwiperSlide key={cast.id}>
+                <StyledCastCard to={`/person/${cast.id}`}>
+                  <img src={buildImageUrl(cast.profile_path, { posterSize: 'w185' })} onError={handleImageError} width={185} height={277} alt={cast.name} />
+                  <p>
+                    <strong>{cast.name}</strong>
+                    <br />
+                    {cast.character}
+                  </p>
+                </StyledCastCard>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </StyledCastList>
     </StyledCredits>
   );
